8341bfacb404d1fe28a1dacd1f631b8e1cec15bb
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-10 17:43:49 -0300

    update tela inicial

--- tecplussystem/lib/views/telaInicial.dart
+++ tecplussystem/lib/views/telaInicial.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tecplussystem/utils/paleta_cores.dart';
+import 'package:tecplussystem/views/tela_principal.dart';
 
 class TelaInicial extends StatelessWidget {
   const TelaInicial({Key? key}) : super(key: key);
@@ -9,20 +11,34 @@ class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _tabs = <Tab> [
-const Tab(text: 'Os',),
-const Tab(text: 'Clientes',),
-const Tab(text: 'Usuários',),
-const Tab(text: 'Caixa',),
-const Tab(text: 'Produtos',),
-const Tab(text: 'Serviços',),
+const Tab(icon: Icon(Icons.file_copy), text: 'O.S' , ),
+const Tab(icon: Icon(Icons.people), text: 'Clientes',),
+const Tab(icon: Icon(Icons.supervised_user_circle_rounded),text: 'Usuários',),
+const Tab(icon: Icon(Icons.attach_money), text: 'Caixa',),
+const Tab(icon: Icon(Icons.production_quantity_limits_sharp),text: 'Produtos',),
+const Tab(icon: Icon(Icons.workspaces_filled),text: 'Serviços',),
+    ];
+
+    final _tabViews = <Widget>[
+      const TelaPrincipal(),
+      const Scaffold (body:Center(child: Text('Clientes'))), 
+      const Scaffold (body:Center(child: Text('Usuários'))),
+      const Scaffold (body:Center(child: Text('Caixa'))),
+      const Scaffold (body:Center(child: Text('Produtos'))),
+      const Scaffold (body:Center(child: Text('Serviços'))),
     ];
     return DefaultTabController(
-        length: 6,
+        length: _tabs.length,
         child:Scaffold( 
           
         appBar: AppBar(
-          bottom: TabBar(tabs: _tabs),
+          title:const Center(child: Text('TecPlus-System')),
+          backgroundColor: PaletaCores.corFundoBotao,
+          bottom: TabBar(
+            labelColor: Colors.white,
+            tabs: _tabs ),
         ),
+        body: TabBarView(children: _tabViews),
       ),
     );
   }
--- tecplussystem/lib/views/tela_principal.dart
+++ tecplussystem/lib/views/tela_principal.dart
@@ -35,215 +35,138 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
               color: PaletaCores.corFundoAba,
               width: larguraTela,
               height: alturaTela,
-              child: Row(
+              child: Column(
                 children: [
-                  // barra lateral
-                  Container(
-                    color: PaletaCores.corPrimaria,
-                    width: larguraTela * 0.2,
-                    height: alturaTela,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    child: Column(
-                      children: [
-                        Expanded(child: Container()),
-                        Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            onTap: () {},
-                            child: Container(
-                                child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: BotaoMenu(texto: 'OS'),
-                            )),
-                          ),
-                        ),
-                        Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              onTap: () {},
-                              child: Container(
-                                  child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: BotaoMenu(texto: 'Clientes'),
-                              )),
-                            )),
-                        Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: BotaoMenu(texto: 'Usuários'),
-                                )))),
-                        Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: BotaoMenu(texto: 'Caixa'),
-                                )))),
-                        Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: BotaoMenu(texto: 'Produtos'),
-                                )))),
-                        Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                                onTap: () {},
-                                child: Container(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: BotaoMenu(texto: 'Serviços'),
-                                )))),
-                        Expanded(child: Container()),
-                      ],
-                    ),
+                  const SizedBox(
+                    height: 5,
                   ),
-                  SizedBox(
-                    width: larguraTela * 0.1,
-                  ),
-                  Column(
-                    children: [
-                      const SizedBox(
-                        height: 25,
-                      ),
 
-                      // Linha com 2 botoes
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          // Botao ADD
-                          Material(
-                            color: Colors.transparent,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(50)),
-                              ),
-                              elevation: 5,
-                              color: PaletaCores.corPrimaria,
-                              child: Container(
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  color: PaletaCores.corPrimaria,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(50)),
-                                ),
-                                child: RawMaterialButton(
-                                    shape: CircleBorder(),
-                                    onPressed: () {},
-                                    child: Icon(
-                                      Icons.add,
-                                      size: 30,
-                                      color: Colors.white,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 2,
-                          ),
-                          // Botão Pesquisar
-                          Material(
-                            color: Colors.transparent,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(50)),
-                              ),
-                              elevation: 5,
-                              color: PaletaCores.corPrimaria,
-                              child: Container(
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  color: PaletaCores.corPrimaria,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(50)),
-                                ),
-                                child: RawMaterialButton(
-                                    shape: CircleBorder(),
-                                    onPressed: () {},
-                                    child: Icon(
-                                      Icons.search_rounded,
-                                      size: 30,
-                                      color: Colors.white,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 25,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      // Tabela de OS
-                      SingleChildScrollView(
+                  // Linha com 2 botoes
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      // Botao ADD
+                      Material(
+                        color: Colors.transparent,
                         child: Card(
-                          color: PaletaCores.corPrimaria,
-                          elevation: 8,
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50)),
                           ),
+                          elevation: 5,
+                          color: PaletaCores.corPrimaria,
                           child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 10),
-                            width: larguraTela * 0.6,
-                            height: alturaTela * 0.7,
+                            width: 50,
+                            height: 50,
                             decoration: const BoxDecoration(
                               color: PaletaCores.corPrimaria,
                               borderRadius:
-                                  BorderRadius.all(Radius.circular(25)),
+                                  BorderRadius.all(Radius.circular(50)),
                             ),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    // ignore: prefer_const_literals_to_create_immutables
-                                    children: [
-                                      Text(
-                                        'Aberto',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          color: Colors.white,
-                                          decoration: TextDecoration.none,
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.all(5),
-                                        width: 10,
-                                        height: 10,
-                                        decoration: BoxDecoration(
-                                          color: Colors.green[800],
-                                          borderRadius: const BorderRadius.all(
-                                              Radius.circular(50)),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                ListaOs(os: os),
-                              ],
-                            ),
+                            child: RawMaterialButton(
+                                shape: CircleBorder(),
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.add,
+                                  size: 30,
+                                  color: Colors.white,
+                                )),
                           ),
                         ),
-                      )
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      // Botão Pesquisar
+                      Material(
+                        color: Colors.transparent,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50)),
+                          ),
+                          elevation: 5,
+                          color: PaletaCores.corPrimaria,
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: PaletaCores.corPrimaria,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: RawMaterialButton(
+                                shape: CircleBorder(),
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.search_rounded,
+                                  size: 30,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                      ),
+                       const SizedBox(
+                    width: 20,
+                  ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  // Tabela de OS
+                  SingleChildScrollView(
+                    child: Card(
+                      color: PaletaCores.corPrimaria,
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 10),
+                        width: larguraTela * 0.9,
+                        height: alturaTela * 0.6,
+                        decoration: const BoxDecoration(
+                          color: PaletaCores.corPrimaria,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25)),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  Text(
+                                    'Aberto',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      decoration: TextDecoration.none,
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.all(5),
+                                    width: 10,
+                                    height: 10,
+                                    decoration: BoxDecoration(
+                                      color: Colors.green[800],
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(50)),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            ListaOs(os: os),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
